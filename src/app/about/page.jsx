7be43ae32feb926f500298131510c60d8d627c11
MD AL AMIN ISLAM <mdalamin212104@gmail.com>
2024-09-16 22:32:55 +0600

    Next js Project setup and Navbar section added

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div className='min-h-screen'>
+            <h1 className='text-5xl font-bold text-center py-5'>this is about page section</h1>
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const dashboard = () => {
+    return (
+        <div className='min-h-screen'>
+            <h1 className=''>this is a dashboard section</h1>
+        </div>
+    );
+};
+
+export default dashboard;
