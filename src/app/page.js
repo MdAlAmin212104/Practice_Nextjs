cb6b1b6cf89aeda4336c07d754feb831edd6197a
MD AL AMIN ISLAM <mdalamin212104@gmail.com>
2024-10-19 22:24:28 +0600

    display date on client site

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,16 @@
 
 export default function Home() {
+  const getDate = async()=> {
+    const res = await fetch('http://localhost:3000/time')
+    const data = await res.json();
+    return data.currentDate;
+  }
+
+
   return (
     <div className="min-h-screen">
       <h1 className="text-4xl font-bold text-center">this is Landing Page here</h1>
+      <h3 className="text-4xl font-bold">Time : {getDate()}</h3>
     </div>
   );
 }
--- src/app/time/route.js
+++ src/app/time/route.js
@@ -1,3 +1,4 @@
+
 export async function GET() {
     return Response.json({
         currentDate : new Date().toLocaleDateString(),
